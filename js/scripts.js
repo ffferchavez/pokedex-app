0cd8ecb8d9f7a3cdb08948efa993878ba5be94ce
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2023-08-30 00:36:03 +0200

    Formatted code with Priettier

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,13 @@
 // This array contains Pokémon data to display in your application.
 
 let pokemonList = [
-    // Adding pokemon objects to the array.
-{ name: "Bulbasaur", height: 7, type: ['grass','poison'] },
-{ name: "Ivyaur", height: 10, type: ['grass','poison'] },
-{ name: "Venusaur", height: 20, type: ['grass','poison'] },
-{ name: "Charmander", height: 6, type: "fire" },
-{ name: "Charmeleon", height: 11, type: "fire"},
-{ name: "Charizard", height: 17, type: "fire" }
+  // Adding pokemon objects to the array.
+  { name: "Bulbasaur", height: 7, type: ["grass", "poison"] },
+  { name: "Ivyaur", height: 10, type: ["grass", "poison"] },
+  { name: "Venusaur", height: 20, type: ["grass", "poison"] },
+  { name: "Charmander", height: 6, type: "fire" },
+  { name: "Charmeleon", height: 11, type: "fire" },
+  { name: "Charizard", height: 17, type: "fire" },
 ];
 
 console.log(pokemonList);
