ff980d8f4d506754c3b038b72699e056bd7beb8c
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2023-08-30 00:24:49 +0200

    Updated app title name

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
-alert('Hello world');
+// Create a new variable called pokemonList and assign to it a blank array.
+// This array contains Pokémon data to display in your application.
 
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
+let pokemonList = [
+    // Adding pokemon objects to the array.
+{ name: "Bulbasaur", height: 7, type: ['grass','poison'] },
+{ name: "Ivyaur", height: 10, type: ['grass','poison'] },
+{ name: "Venusaur", height: 20, type: ['grass','poison'] },
+{ name: "Charmander", height: 6, type: "fire" },
+{ name: "Charmeleon", height: 11, type: "fire"},
+{ name: "Charizard", height: 17, type: "fire" }
+];
+
+console.log(pokemonList);
