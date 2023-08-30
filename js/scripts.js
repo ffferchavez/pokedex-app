957e33d06b087c5d5687bb2a7278f7e16cd70a65
ffferchavez <138360629+ffferchavez@users.noreply.github.com>
2023-08-30 23:08:07 +0200

    Fixed array objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
 // Create a new variable called pokemonList and assign to it a blank array.
 // This array contains Pokémon data to display in your application.
 
-let pokemonList = [
-  // Adding pokemon objects to the array.
-  { name: "Bulbasaur", height: 7, type: ["grass", "poison"] },
-  { name: "Ivyaur", height: 10, type: ["grass", "poison"] },
-  { name: "Venusaur", height: 20, type: ["grass", "poison"] },
-  { name: "Charmander", height: 6, type: "fire" },
-  { name: "Charmeleon", height: 11, type: "fire" },
-  { name: "Charizard", height: 17, type: "fire" },
-];
+let pokemonList = [];
 
-console.log(pokemonList);
+// Add Pokémon objects to the array
+pokemonList[0] = {
+  name: "Bulbasaur",
+  height: 7,
+  type: ["grass", "poison"]
+};
+
+pokemonList[1] = {
+  name: "Ivysaur",
+  height: 10,
+  type: ["grass", "poison"]
+};
+
+pokemonList[2] = {
+  name: "Venusaur",
+  height: 20,
+  type: ["grass", "poison"]
+};
+
+pokemonList[3] = {
+  name: "Charmander",
+  height: 6,
+  type: ["fire"]
+};
+
+pokemonList[4] = {
+  name: "Charmeleon",
+  height: 11,
+  type: ["fire"]
+};
+
+pokemonList[5] = {
+  name: "Charizard",
+  height: 17,
+  type: ["fire"]
+};
